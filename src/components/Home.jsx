beaf6d82615e1ba471121d4d721d72a07fd13b1d
satyamJha002 <satyam073000@gmail.com>
2024-06-27 09:28:58 +0530

    modified home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-3 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="mt-20 md:mt-0 text-2xl sm:text-7xl font-bold text-white">
+          <h2 className="mt-20 md:mt-0 text-4xl sm:text-7xl font-bold text-white">
             <TypeWriter text="Frontend Developer" delay={85} />
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
