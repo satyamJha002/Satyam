88475e0d38c656558416789bc9ee8251d5c24e1d
satyamJha002 <satyam073000@gmail.com>
2024-08-23 13:16:57 +0530

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,16 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
-import { Link } from "react-scroll";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home />
-      <Link to="about" smooth={true} duration={500}>
-        <About />
-      </Link>
-
-      <Link to="portfolio" smooth={true} duration={500}>
-        <Portfolio />
-      </Link>
-
-      <Link to="skills" smooth={true} duration={500}>
-        <Skills />
-      </Link>
-
-      <Link to="contact" smooth={true} duration={500}>
-        <Contact />
-      </Link>
-
+      <About />
+      <Portfolio />
+      <Skills />
+      <Contact />
       <SocialLinks />
     </div>
   );
